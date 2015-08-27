48782322c7273f5b90d86a5dba69efd50b589ddf
Corbin Otwell <cbotwell@gmail.com>
2015-08-27 16:53:11 -0500

    adds reset, bourbon, neat, scss styles

--- app/components/forecast-viewer.js
+++ app/components/forecast-viewer.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    showWeek: function() {
+      this.$('.week').slideToggle();
+    },
+    showDay: function() {
+      this.$('.day').slideToggle();
+    },
+  },
 });
