9686b67a8efe146c987545e4721a4ea41a8325f4
Corbin Otwell <cbotwell@gmail.com>
2015-08-26 22:10:39 -0500

    adds content security policy

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'weather-app',
+    contentSecurityPolicy: { 'connect-src': "'self' https://safe-river-8896.herokuapp.com/" },
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
